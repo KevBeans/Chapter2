0ee09bbce603ac9006d7bd835817e4580af24fe5
Valter Melon <kevooi.of.estland@gmail.com>
2016-11-13 01:23:40 +0200

    Initial commit

---
+++ src/com/company/Ch22.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Ch22 {
+
+    public static void main(String[] args) {
+
+        String day = "Friday";
+        String date = "13";
+        String month = "June";
+        int year = 1993;
+
+        System.out.println("American format:");
+        System.out.println(day + ", " + month + " " + date + ", " + year + ".");
+        System.out.println("European format:");
+        System.out.println(day + " " + date + " " + month + " " + year + ".");
+    }
+}
